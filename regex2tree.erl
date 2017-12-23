7c3ed78b125849496cd79bb78f3be792c393beed
Lukasz Sciga <lsciga@akamai.com>
2017-12-23 01:58:32 +0100

    removed old non-used code

--- regex2tree.erl
+++ regex2tree.erl
@@ -3,27 +3,13 @@
 %% API
 -export([re2post/1, extract_from_parentheses/1]).
 
-
-re2post2(Input) -> re2post(Input, []).
-
-re2post2([], Output) -> Output;
-re2post2(Input, Output) ->
-  case string:slice(Input, 0, 1) of
-    "+" -> re2post(string:slice(Input, 1, string:length(Input)), lists:reverse(["+."|Output])); % gonna reverse
-    "*" -> re2post(string:slice(Input, 1, string:length(Input)), lists:reverse(["*."|Output])); % gonna reverse
-    "(" -> InsideParentheses = extract_from_parentheses(Input),
-      PostfixOfInside = lists:reverse(["."|re2post(InsideParentheses)]),
-      re2post(string:slice(Input, string:length(InsideParentheses) + 2), lists:merge(Output, PostfixOfInside));
-    CharLiteral -> re2post(string:slice(Input, 1, string:length(Input)), lists:reverse([CharLiteral|Output]))
-end.
-
 re2post(Input) -> re2post(Input, []).
 
 re2post([], Output) -> Output;
 re2post(Input, Output) ->
   case string:slice(Input, 0, 1) of
     "+" -> re2post(string:slice(Input, 1, string:length(Input)), lists:append(Output, ["+","."]));
-    "*" -> re2post(string:slice(Input, 1, string:length(Input)), lists:append(Output, ["*","."])); % gonna reverse
+    "*" -> re2post(string:slice(Input, 1, string:length(Input)), lists:append(Output, ["*","."]));
     "(" -> InsideParentheses = extract_from_parentheses(Input),
       PostfixOfInside = lists:append(re2post(InsideParentheses), ["."]),
       re2post(string:slice(Input, string:length(InsideParentheses) + 2), lists:merge(Output, PostfixOfInside));
