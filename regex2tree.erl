9f7a1b989497027345fa2e5c26a280b845c52993
Kuba Gwizdała <jakub.gwizdala@gmail.com>
2017-12-23 18:51:00 +0100

    Reimplemented re2post function

--- regex2tree.erl
+++ regex2tree.erl
@@ -2,37 +2,75 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% API
--export([re2post/1, extract_from_parentheses/1]).
+-export([re2post/1,
+         extract_from_parentheses/1]).
 
-re2post(Input) -> re2post(Input, []).
+re2post([]) -> [];
+re2post(Input) -> re2post(Input, 0, [], []).
 
-re2post([], Output) -> Output;
-re2post(Input, Output) ->
-  case string:slice(Input, 0, 1) of
-    "+" -> re2post(string:slice(Input, 1, string:length(Input)), lists:append(Output, ["+","."]));
-    "*" -> re2post(string:slice(Input, 1, string:length(Input)), lists:append(Output, ["*","."]));
-    "(" -> InsideParentheses = extract_from_parentheses(Input),
-      PostfixOfInside = lists:append(re2post(InsideParentheses), ["."]),
-      re2post(string:slice(Input, string:length(InsideParentheses) + 2), lists:merge(Output, PostfixOfInside));
-    CharLiteral -> re2post(string:slice(Input, 1, string:length(Input)), lists:append(Output, [CharLiteral]))
-end.
+re2post([], WrittenCount, OperatorStack, Output) ->
+  PostfixNotation = lists:append(lists:reverse(OperatorStack), Output),
+  case (length(OperatorStack) =:= 0) and (WrittenCount > 1) of
+    true -> Result = concatenation(PostfixNotation);
+    false -> Result = PostfixNotation
+  end,
+  lists:reverse(Result);
+re2post([Char | Input], WrittenCount, OperatorStack, Output) ->
+  case is_unary_operator(Char) of
+    true -> re2post(Input, WrittenCount, OperatorStack, [Char | Output]);
+    false ->
+      if
+        WrittenCount =:= 2 ->
+          case length(OperatorStack) > 0 of
+            true ->
+              [Operator | LeftOperators] = OperatorStack,
+              re2post([Char | Input], 1, LeftOperators, [Operator | Output]);
+            false ->
+              re2post([Char | Input], 1, OperatorStack, concatenation(Output))
+          end;
+        true -> case Char of
+            $( ->
+              [Inside, Outside] = extract_from_parentheses(Input),
+              PostfixOfInside = lists:reverse(re2post(Inside)),
+              NewOutput = lists:append(PostfixOfInside, Output),
+              re2post(Outside, WrittenCount + 1, OperatorStack, NewOutput);
+            BinaryOperator when BinaryOperator =:= $+ ->
+              re2post(Input, WrittenCount, [BinaryOperator | OperatorStack], Output);
+            UnaryOperator when UnaryOperator =:= $* ->
+              re2post(Input, WrittenCount, OperatorStack, [UnaryOperator | Output]);
+            CharLiteral ->
+              re2post(Input, WrittenCount + 1, OperatorStack, [CharLiteral | Output])
+          end
+      end
+  end.
 
-extract_from_parentheses(String) -> [Inside|_] = string:split(string:slice(String, 1), ")", trailing), Inside.
+extract_from_parentheses(Input) -> extract_from_parentheses(Input, [], 0).
+
+extract_from_parentheses([Char | Input], Inside, Count) ->
+  case Char of
+    $( -> extract_from_parentheses(Input, [Char | Inside], Count + 1);
+    $) -> case Count of
+             0 -> [lists:reverse(Inside), Input];
+             _ -> extract_from_parentheses(Input, [Char | Inside], Count - 1)
+           end;
+    NotParentheses -> extract_from_parentheses(Input, [NotParentheses | Inside], Count)
+  end.
+
+is_unary_operator(Char) -> lists:member(Char, [$*]).
+
+concatenation(Output) -> [$. | Output].
 
 
 re2post_test() ->
   [
     ?assert(re2post("") =:= []),
-    ?assert(re2post("a") =:= ["a"]),
-    ?assert(re2post("a(bb)") =:= ["a","b","b","."]),
-    ?assert(re2post("a+c") =:= ["a","+",".","c"])
-  ].
-
-extract_from_parentheses_test() ->
-  [
-    ?_assert(extract_from_parentheses("()") =:= ""),
-    ?_assert(extract_from_parentheses("(a)") =:= "a"),
-    ?_assert(extract_from_parentheses("(ab)") =:= "ab"),
-    ?_assert(extract_from_parentheses("(asdf)") =:= "asdf"),
-    ?_assert(extract_from_parentheses("((dupa)a)") =:= "(dupa)a")
+    ?assert(re2post("a") =:= "a"),
+    ?assert(re2post("(a)") =:= "a"),
+    ?assert(re2post("((a))") =:= "a"),
+    ?assert(re2post("aaa") =:= "aa.a."),
+    ?assert(re2post("a(bb)") =:= "abb.."),
+    ?assert(re2post("a+c") =:= "ac+"),
+    ?assert(re2post("b+c+d") =:= "bc+d+"),
+    ?assert(re2post("(aa)+(bb)") =:= "aa.bb.+"),
+    ?assert(re2post("a(b+(cc)*)d(aa)*") =:= "abcc.*+.d.aa.*.")
   ].
