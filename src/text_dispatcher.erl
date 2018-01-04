5b532d2268bf96f03599f64cde43d854ca7cec4d
Lukasz Sciga <lsciga@akamai.com>
2018-01-04 19:12:10 +0100

    fixed splitting text in parts

--- src/text_dispatcher.erl
+++ src/text_dispatcher.erl
@@ -9,9 +9,10 @@ process_count() -> 4.
 dispatch([]) -> empty_text;
 dispatch(Text) -> dispatch(Text, process_count(), 0, string:length(Text), []).
 dispatch(_, 0, _, _, PidList) -> lists:reverse(PidList);
-dispatch(Text, ProcessesLeft, SliceStart, SliceLength, PidList) ->
+dispatch(Text, ProcessesLeft, SliceStart, TextLength, PidList) ->
+  SliceLength = TextLength div ProcessesLeft,
   Pid = spawn(graph_processor, process, [string:slice(Text, SliceStart, SliceLength)]),
-  dispatch(Text, ProcessesLeft - 1, SliceStart + SliceLength, SliceLength, [Pid|PidList]).
+  dispatch(Text, ProcessesLeft - 1, SliceStart + SliceLength, TextLength - SliceLength, [Pid|PidList]).
 
 %% TEST
 process_count_test() ->
